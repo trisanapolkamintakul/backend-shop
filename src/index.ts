210eb1bb055c0069c49805057a4ec43fdae3dbf5
trisk505 <trisk505@aparr.postbox.in.th>
2018-09-18 08:46:55 +0700

    motify

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world");
+import { Server, ResponseToolkit, Request } from "hapi";
+
+
+const server = new Server({
+    port: "5000"
+});
+server.route([
+    {
+        path: "/",
+        method: "GET",
+        handler: (request: Request, h: ResponseToolkit) => {
+return "Hello World";
+        }
+    }
+]);
+
+server.start().then(
+    () => {console.log("Server start"); },
+    (err) =>{console.log("Server error" + err);}
+);
